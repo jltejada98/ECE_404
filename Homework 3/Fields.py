fb66d49c04e7c76f2f627f424ee6ffd1c9f7063a
jltejada98 <tejada@purdue.edu>
2020-02-04 14:24:34 -0500

    Homework 3 (Faster runtime)

--- Homework 3/Fields.py
+++ Homework 3/Fields.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 def determine_field(n):
-    Z_n = [x for x in range(n) if x !=0 ]
     #For each element, determine if all elements of Zn -excluding 0- have a multiplicative inverse
     #We can do this by determining whether an element is relatively prime to n.
-    for z in Z_n:
+    z_index = 1
+    while(z_index < n):
         #Perform gcd(z,k=n) and determine if == 1, if so there is a multiplicative inverse,
         #Otherwise there is not, which means it is a Comunitative ring.
         k = n
+        z = z_index
         while(k):
             z,k = k, z % k
         if(z != 1):
             print("ring")
             return
+        z_index += 1
     print("field")
     return
 
