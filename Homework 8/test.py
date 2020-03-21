e54f6b1cf026f72b4eb40e3d03ad46b9b00bd469
jltejada98 <tejada@purdue.edu>
2020-03-21 02:43:14 -0400

    Homework 8 (Added output file for testing port scanning and attacking)

--- Homework 8/test.py
+++ Homework 8/test.py
@@ -1,9 +1,9 @@
 from TcpAttack import *
 #Your TcpAttack class should be named as TcpAttack
-spoofIP="10.0.0.54" ; targetIP="10.0.0.229" #Will contain actual IP addresses in real script
-rangeStart=0 ; rangeEnd=1024 ; port=80
+spoofIP="10.0.0.10" ; targetIP="10.0.0.229" #Will contain actual IP addresses in real script
+rangeStart=78; rangeEnd=82; port=80
 Tcp = TcpAttack(spoofIP,targetIP)
-# Tcp.scanTarget(rangeStart, rangeEnd)
+Tcp.scanTarget(rangeStart, rangeEnd)
 if Tcp.attackTarget(port,10):
        print("port was open to attack")
 else:
